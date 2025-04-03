407e92da15710d475fd2469039475ebd31c9d89c
Titouan Martineau <mrt5050a@ensfsi.onmicrosoft.com>
2025-04-03 17:12:35 +0200

    ça marche

--- test/controleur/ControlLibererEtalTest.java
+++ test/controleur/ControlLibererEtalTest.java
@@ -2,10 +2,17 @@ package controleur;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ControlLibererEtalTest {
+	
 
+	@BeforeEach
+	public void init() {
+		
+	}
+	
 	@Test
 	void testControlLibererEtal() {
 		fail("Not yet implemented");
